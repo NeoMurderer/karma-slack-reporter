04517e595bba86550e591e8fabb21953204dc5dd
Oleg Nesterov <oleh.nesterov@gmail.com>
2016-12-06 17:11:30 +0200

    Setup send report to slack after finish tests

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
-const chalk_global = require('chalk');
 const Slack = require('slack-node');
-const apiToken = "xoxb-112438842800-U4g07vrFe0DzpiDJywZs9w1z";
 
-const slack = new Slack(apiToken);
+
+
+function strmul(s, n) {
+	var r = '';
+	for (var i = 0; i < n; ++i) {
+		r += s;
+	}
+	return r;
+}
+function bold(str) {
+  return `*${str}*`;
+}
+function italic(str) {
+  return `_${str}_`;
+}
 const SlackReporter = function(baseReporterDecorator, config) {
     baseReporterDecorator(this);
-
     // Configuration
-    config.summaryReporter = config.summaryReporter || {};
-    var show = config.summaryReporter.show || 'failed';
-    var specLength = config.summaryReporter.specLength || 50;
-    var overviewColumn = config.summaryReporter.overviewColumn === false ? false : true;
+    config.slackReporter = config.slackReporter || {};
+    var show = config.slackReporter.show || 'failed';
+    var specLength = config.slackReporter.specLength || 50;
+    var overviewColumn = config.slackReporter.overviewColumn === false ? false : true;
 
-    // We use our own instance, respecting config.color
-    var chalk = new chalk_global.constructor({
-        enabled: config.colors
-    });
-
+		const apiToken = config.slackReporter.apiToken;
+		const slack = new Slack(apiToken);
     var specorder, specresults;
 
     this.specSuccess = this.specFailure = this.specSkipped = function(browser, result) {
         if (!result.suite) {
             return;
         }
-
         var specid = result.suite.join('/') + '/' + result.description;
         if (!(specid in specresults)) {
             specorder.push(specid);
@@ -36,57 +43,34 @@ const SlackReporter = function(baseReporterDecorator, config) {
     }
 
     // Previously printed spec path
-    var currentPath;
-    this.printSpecLabel = function(path) {
-        var indent = "  ";
-        path.forEach(function(s, i) {
-            // We keep the current comon prefix and start to print
-            // the new information on the first difference.
-            if (i < currentPath.length && s != currentPath[i]) {
-                currentPath.length = i;
-            }
-            if (i >= currentPath.length) {
-                var label = indent + s;
-                if (label.length > specLength) {
-                    label = label.slice(0, specLength - 3) + '...';
-                }
-                this.writeCommonMsg(label);
-
-                if (i < path.length - 1) {
-                    this.writeCommonMsg("\n");
-                } else {
-                    this.writeCommonMsg(strmul(' ', specLength - label.length));
-                }
-                currentPath.push(s);
-            }
-            indent += "  ";
-        }, this);
+    var currentPath = {};
+    this.printSpecLabel = function(spec,summary) {
+        if(!currentPath[spec.spec[0]]) currentPath[spec.spec[0]] = [];
+        currentPath[spec.spec[0]].push({
+          test:spec.spec[1].trim(),
+					results:spec.results,
+          summary:summary
+        })
     };
 
-    this.printResultLabel = function(result) {
+    this.getResultLabel = function(result) {
         if (result === undefined)
-            this.writeCommonMsg(chalk.yellow(' ? '));
+            return ' ? ';
         else if (result.skipped)
-            this.writeCommonMsg(chalk.yellow(' - '));
+            return ' - '
         else if (result.success) {
             if (!result.partial)
-                this.writeCommonMsg(chalk.green(' ✓ '));
+                return ' ✓ '
             else
-                this.writeCommonMsg(chalk.yellow('(✓)'));
+                return '(✓)'
         } else {
-            this.writeCommonMsg(chalk.red(' ✗ '));
+            return ' ✗ '
         }
     };
 
-    this.printTableHeader = function(browsers) {
-        this.writeCommonMsg(strmul(' ', specLength));
-        if (overviewColumn) {
-            this.writeCommonMsg(' all  ');
-        }
-        browsers.forEach(function(browser, i) {
-            this.writeCommonMsg(' ' + i + ' ');
-        }, this);
-        this.writeCommonMsg('\n');
+    this.getTableHeader = function(browsers,testTitle) {
+        let message = '' + bold(testTitle) + strmul(' ', specLength  - testTitle.length);
+        return message;
     }
 
     this.onRunStart = function() {
@@ -96,19 +80,19 @@ const SlackReporter = function(baseReporterDecorator, config) {
         specresults = Object.create(null);
     }
 
-    this.onRunComplete = function(browsers, results) {
-        this.writeCommonMsg(chalk.bold(chalk.underline('SUMMARY')) + '\n');
-
+    this.onRunComplete = function(browsers) {
         // Browser overview
-        browsers.forEach(function(browser, i) {
-            this.writeCommonMsg(' ' + i + ': ' + this.renderBrowser(browser) + '\n');
+        let browserMessage = '*Summary:* \n ```'
+        browsers.forEach(function(browser) {
+          browserMessage += this.renderBrowser(browser) + '\n'
         }, this);
+        browserMessage += ' ``` '
+        this.addSlackMessage(browserMessage);
 
         if (!specorder.length) {
-            this.writeCommonMsg(chalk.red('No tests did run in any browsers.'));
+            this.addSlackMessage('No tests did run in any browsers.');
             return;
         }
-
         var tableHeaderShown = false;
 
         // Test details
@@ -139,51 +123,79 @@ const SlackReporter = function(baseReporterDecorator, config) {
 
             if (summary.success) {
                 // Maybe we don't even want to show it
-                if (show == 'failed') {
+                if (show === 'failed') {
                     if (summary.ran_some)
                         counts.hidden++;
                     return;
                 }
-                if (show == 'skipped' && !summary.skipped_some) {
+                if (show === 'skipped' && !summary.skipped_some) {
                     if (summary.ran_some)
-                        counts.hidden++;
+                        counts.skipped++;
                     return;
                 }
             }
 
             // We want to actually display it
             if (!tableHeaderShown) {
-                this.printTableHeader(browsers);
+                // this.printTableHeader(browsers);
                 tableHeaderShown = true;
             }
 
-            this.printSpecLabel(sr.spec);
+            this.printSpecLabel(sr,summary);
             if (overviewColumn) {
-                this.writeCommonMsg(' ');
-                this.printResultLabel(summary);
-                this.writeCommonMsg('  ');
+                // this.writeCommonMsg(' ');
+                // this.printResultLabel(summary);
+                // this.writeCommonMsg('  ');
             }
             browsers.forEach(function(browser, i) {
-                this.printResultLabel(sr.results[browser.id], i);
+              // console.log(i);
+              // this.printResultLabel(sr.results[browser.id], i);
             }, this);
-            this.writeCommonMsg("\n");
+            // this.writeCommonMsg('\n');
             counts.shown++;
         }, this);
 
-        if (counts.hidden) {
-            slack.api('chat.postMessage', {
-                text: "  " + chalk.green('' + counts.hidden) +
-                    (counts.shown ? " more" : "") +
-                    " test cases successful in all browsers\n",
-                channel: '#visumservice-tests'
-            }, function(err, response) {
-                console.log(response);
-            });
-            this.writeCommonMsg("  " + chalk.green('' + counts.hidden) +
-                (counts.shown ? " more" : "") +
-                " test cases successful in all browsers\n");
+        if(currentPath) {
+
+          let message = ''
+          for (let index in currentPath) {
+            message +=  this.getTableHeader(browsers,index) +  '\n'
+            currentPath[index].map((spec) => {
+              message += '`' + spec.test.trim() + strmul(' ', specLength - spec.test.length) + '`'  + '\n'
+            })
+						message += '\n'
+          }
+          this.addSlackMessage(message);
         }
+        if (counts.hidden) {
+            let message = counts.hidden +
+              (counts.shown ? ' more' : '') +
+              ' test cases successful in all browsers\n'
+            this.addSlackMessage(message);
+        }
+				this.sendSlackMessages();
     };
+		let slackMessages = [];
+		this.addSlackMessage = function (message) {
+			slackMessages.push(message);
+		}
+    this.sendSlackMessages = function () {
+			let summaryMessage = '';
+			slackMessages.map( (message) => summaryMessage += message)
+
+      slack.api('chat.postMessage', {
+          text: summaryMessage,
+          channel: config.slackReporter.channel
+      }, (err, response) => {
+          if(response.ok) {
+            this.writeCommonMsg('Report sent to Slack')
+          }
+          else {
+            this.writeCommonMsg('Error sent report to Slack')
+            this.writeCommonMsg(response)
+          }
+      });
+    }
 }
 
 SlackReporter.$inject = [
